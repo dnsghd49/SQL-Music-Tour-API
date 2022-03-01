487c955ce7bf44c4b426be5d69b8e1acadd736f4
Aaron Lu <aaronnoh0409@gmail.com>
2022-02-28 21:58:16 -0500

    checkpoint

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
-
-// CONTROLLERS 
-const bandsController = require('./controllers/bands_controller')
-app.use('/bands', bandsController)
+const { Sequelize } = require('sequelize')
+const bands = require('./controllers/bands_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -17,6 +15,10 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
