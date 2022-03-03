caa9d6aea698ba25765175a205fe5ab8ed6d05e4
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-02 20:19:09 -0500

    check point

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db
+const { Band, MeetGreet } = db
 const { Op } = require('sequelize')
 
 // FIND ALL BANDS
@@ -73,6 +73,36 @@ bands.delete('/:id', async (req, res) => {
         })
     } catch (err) {
         res.status(500).json(err)
+    }
+})
+
+// FIND A SPECIFIC BAND
+bands.get('/:name', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { name: req.params.name },
+            include: [{
+                model: MeetGreet,
+                as: "meet_greets",
+                include: {
+                    model: Event,
+                    as: "event",
+                    where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                }
+            },
+            {
+                model: SetTime,
+                as: "set_times",
+                include: {
+                    model: Event,
+                    as: "event",
+                    where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                }
+            }]
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
     }
 })
 
